9b8e6b8a179f358f175152a1de98021959d7446b
RifqiHanif <59543444+greenslime24@users.noreply.github.com>
2022-11-07 12:14:49 +0700

    Create 8. Weakmap & weakset.js
    learning about weakmap and weakset. weakmap doenst work tho...

---
+++ 2. pengenalan struktur data/8. Weakmap & weakset.js
@@ -0,0 +1,36 @@
+//variant of map yang mendukung garbage collection(interpreter mengambil kembali memori yang tidak lagi dapat dijangkau dan tidak dapat digunakan oleh program)
+//weak dalam weakmap adalah referensi terhadap nilai yang disimpan. apabila nilai yang disimpan sudah tidak terjangkau. maka referensi ke memorinya akan dihapus
+
+//map biasa
+let visitCountMap = new Map();
+
+function counteruser(user){
+    let count = visitCountMap.get(user) || 0;
+    visitCountMap.set(user, count + 1 );
+}
+
+let jonas = {name: "jonas"};
+counteruser(jonas);
+
+jonas = null; //-->mencoba menghapus nilai jonas
+
+setTimeout(() => {
+    console.log(visitCountMap); //--> nilai jonas akan tetap ada. harus benar benar dihapus
+}, 10000);
+
+//weakmap
+const { inspect } = require('util');
+let visitcountweakmap = new WeakMap();
+
+function countUser(user) {
+    let count = visitcountweakmap.get(user) || 0;
+    visitcountweakmap.set(user, count + 1);
+}
+
+let akbar = {nama: "akbar"};
+countUser(akbar);
+
+akbar = null;
+setTimeout(() => {
+    console.log(inspect(visitcountweakmap, {showHidden: true})); //--> 1) nilai weakmap tidak dapat dilihat secara langsung(use inspect) nilai akbar akan terhapus
+}, 10000); //-->dibutuhkan untuk membeirkan waktu kerja pada garbage collector
